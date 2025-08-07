f5831f858d578496e28d1bbd3974977a3aa2ccf2
Morlok8~ <maik3786@gmail.com>
2025-08-07 17:11:14 +0300

    change to controllers

--- app/Http/Controllers/StocksController.php
+++ app/Http/Controllers/StocksController.php
@@ -2,9 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Collection;
+
+
+use App\Models\Stocks;
 
 class StocksController extends Controller
 {
-    //
+    // 
+    public function show(Request $request){
+        $validated = $request->validate([
+            'page' => 'numeric',
+            'limit' => 'numeric|max:500',
+            'dateTo' => 'required|date_format:Y-m-d',
+        ]);
+
+        $currentDate = Carbon::now()->format('Y-m-d');
+
+        $page = $request->page;
+
+        $limit = $request->limit;
+
+        $date_to = $request->dateTo;
+
+        $response = Http::get('http://109.73.206.144:6969/api/stocks?dateFrom='.$currentDate.'&dateTo='.$date_to.'&page='.$page.'&key=E6kUTYrYwZq2tN4QEtyzsbEBk3ie&limit='.$limit);
+
+        $data = $response->body();
+
+        $phpObject = json_decode($data);
+
+        $new_data = array_map(function($item) {
+            return (array)$item; 
+        }, $phpObject->data);
+
+        Stocks::upsert($new_data, ['sc_code', 'nm_id', 'date', 'warehouse_name', 'supplier_article']);
+
+        return response()->json([
+            "data" => $phpObject->data,
+            "request" => $request->page
+        ], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\StocksController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::get('/show', [OrdersController::class, 'show']);
+Route::name('stock')->group(function(){
+    Route::get('stock/show', [StocksController::class, 'show']);
+});
